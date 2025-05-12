dfb700e539a8d27827b4a40698d8be046326bf88
VitaliaKr <81364950+VitaliaKr@users.noreply.github.com>
2025-05-12 20:23:20 +0400

    Task 3 done

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //todo ФИО
+        String inputData = inputString();
+        if (isValidString(inputData)) {
+            System.out.println("Фамилия: " + lastName(inputData));
+            System.out.println("Имя: " + fullName(inputData));
+            System.out.println("Отчество: " + middleName(inputData));
+        } else {
+            System.out.println("Введенная строка не является ФИО");
+        }
+    }
+
+    public static String inputString() {
+        System.out.println("Введите ФИО: ");
+        return new Scanner(System.in).nextLine().trim();
+    }
+
+    public static boolean isValidString(String validateString) {
+        boolean isValid = validateString.matches("[А-яё-]+\\s[А-яё-]+\\s[А-яё-]+");
+        return isValid;
+    }
+
+    public static String lastName(String inputString) {
+        return inputString.substring(0, inputString.indexOf(' ')).trim();
+    }
+
+    public static String fullName(String inputString) {
+        return inputString.substring(inputString.indexOf(' '), inputString.lastIndexOf(' ')).trim();
+    }
+
+    public static String middleName(String inputString) {
+        return inputString.substring(inputString.lastIndexOf(' ')).trim();
     }
 }
